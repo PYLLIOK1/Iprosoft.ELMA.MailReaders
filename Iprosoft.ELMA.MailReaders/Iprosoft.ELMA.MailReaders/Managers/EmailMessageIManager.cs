40822ffb83f7d48b36361ced63a51f40725e0ed8
Максим Новиков <pylliok1@mail.ru>
2020-04-25 14:21:04 +0300

    1111

--- Iprosoft.ELMA.MailReaders/Iprosoft.ELMA.MailReaders/Managers/EmailMessageIManager.cs
+++ Iprosoft.ELMA.MailReaders/Iprosoft.ELMA.MailReaders/Managers/EmailMessageIManager.cs
@@ -1,4 +1,6 @@
-﻿using EleWise.ELMA.ComponentModel;
+﻿using EleWise.ELMA.Actions;
+using EleWise.ELMA.ComponentModel;
+using EleWise.ELMA.Extensions;
 using EleWise.ELMA.Model.Managers;
 using Iprosoft.ELMA.MailReaderConf.Models;
 using System;
@@ -24,6 +26,62 @@ namespace Iprosoft.ELMA.MailReaders.Managers
                 .SetParameter("OtKogo", OtKogo)
                 .SetTimeout(0).UniqueResult<long>();
             return Instance.Load(IdResult);
+        }
+        /// <summary>
+        /// Методы не работают
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public override ActionCheckResult CanDelete(IEMailMessageI obj)
+        {
+            return obj != null
+                ? new ActionCheckResult(false, "Запись нельзя удалить!")
+                { Result = false, ErrorMessage = "Запись нельзя удалить!" }
+                : base.CanDelete(obj);
+        }
+
+        public override void Delete(IEMailMessageI obj)
+        {
+
+            if (obj != null) throw new Exception("Запись нельзя удалить!");
+            base.Delete(obj);
+        }
+
+        public override void Save(IEMailMessageI obj)
+        {
+            var findedEMailMessage = LoadOrNull(GetUniqueGuid(obj));
+            if (obj.IsNew())
+            {
+                if (findedEMailMessage != null)
+                    throw new Exception("Такая запись уже есть!");
+            }
+            else
+            {
+                if (findedEMailMessage != null && obj.Id != findedEMailMessage.Id)
+                    throw new Exception("Такая запись уже есть!");
+            }
+            obj.Uid = GetUniqueGuid(obj);
+
+            base.Save(obj);
+        }
+
+        /// <summary>
+        /// Методы не работают
+        /// </summary>
+        /// <param name="obj">The object.</param>
+        /// <returns></returns>
+        public override ActionCheckResult CanSave(IEMailMessageI obj)
+        {
+            var findedEMailMessage = LoadOrNull(GetUniqueGuid(obj));
+            return findedEMailMessage != null
+                ? new ActionCheckResult(false, "Такая запись уже есть!")
+                { Result = false, ErrorMessage = "Такая запись уже есть!" }
+                : base.CanSave(obj);
+        }
+
+        private Guid GetUniqueGuid(IEMailMessageI obj)
+        {
+            return $"{obj}".GetDeterministicGuid();
         }
     }
 }
